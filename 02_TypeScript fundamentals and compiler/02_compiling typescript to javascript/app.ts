1f22d6902bb9d0000b55cf0f65324cf0ef324e12
Adipi Manojkumar <adipimanojkumar@gmail.com>
2026-06-17 16:33:22 +0530

    what are types

---
+++ 02_TypeScript fundamentals and compiler/02_compiling typescript to javascript/app.ts
@@ -0,0 +1,14 @@
+// to install type script use "npm i -g typescript"
+
+let num: string = 3;
+console.log(typeof num);
+num = 5;
+console.log(typeof num);
+num = "string";
+console.log(typeof num);
+const obj = {
+  name: "manoj",
+};
+console.log(typeof obj);
+console.log("hellos");
+console.log("hii");
